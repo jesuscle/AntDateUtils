b0d8447f63ba74bb7a0de00c1f415db57d184386
jesuscle <jesus.salinas@cleformacion.com>
2021-09-22 13:46:00 +0200

    Cambio menor

--- src/com/mkyong/core/utils/DateUtils.java
+++ src/com/mkyong/core/utils/DateUtils.java
@@ -5,9 +5,7 @@ import java.util.Date;
 public class DateUtils {
 
 	public static void main(String[] args) {
-
 		System.out.println(getLocalCurrentDate());
-		
 	}
 
 	private static Date getLocalCurrentDate() {
